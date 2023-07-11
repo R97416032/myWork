449fcadb9a076f721362ae2f98038b0cab9498c6
Monster-R <97416032@qq.com>
2023-07-11 14:14:04 +0800

    项目文件

---
+++ test/decompressDemo.py
@@ -0,0 +1,8 @@
+
+from utils.decompress import *
+
+path="../data/compress/GSE113660_RAW.tar"
+un_tar(path)
+paths=os.listdir(path.replace(".tar",""))
+for p in paths:
+    un_gz(path.replace(".tar","/")+p)
