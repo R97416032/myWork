d8a48590e64a471879c39819bae3fed001f1baba
Monster-R <97416032@qq.com>
2023-07-19 15:49:34 +0800

    解压工具

---
+++ data/compress/GSE113660/GSE113660_RAW/ungz.py
@@ -0,0 +1,4 @@
+from utils.decompress import un_gz
+un_gz("GSM3110765_genes.tsv.gz")
+un_gz("GSM3110765_barcodes.tsv.gz")
+un_gz("GSM3110765_matrix.mtx.gz")
---
+++ data/compress/GSE113660/untar.py
@@ -0,0 +1,3 @@
+from utils.decompress import un_tar
+
+un_tar("GSE113660_RAW.tar")
